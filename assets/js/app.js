36cd53df278b48f0a52c258fab520f871707e7c1
Javtesh <javteshsingh.bhullar@georgebrown.ca>
2021-07-11 14:39:40 +0530

    javtesh_added_audio

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,20 @@
 function togglePopup(){
     document.getElementById("popup-1").classList.toggle("active");
   }
+
+
+function playMe(){
+    var audio = document.getElementById('testAudio');
+
+    if(this.className == 'is-playing'){
+      this.className = "";
+      document.getElementById("playAudio").innerHTML = "Play &#9658;"
+      audio.pause();
+    }else{
+      this.className = "is-playing";
+      document.getElementById("playAudio").innerHTML = "Pause &#9208;";
+      audio.play();
+    }
+}
+
+
