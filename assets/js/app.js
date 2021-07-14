fe15f9371b8d37be7f47a35365e07d0f06802443
Javtesh <javteshsingh.bhullar@georgebrown.ca>
2021-07-15 00:41:03 +0530

    bug_fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,40 @@
 const AUDIO_KEY = "math-game-audio"
 
 passvalues = () => {
-  var firstname = document.getElementById("txt").value;
-  localStorage.setItem("textvalue",firstname);
-  return false;
+  if (document.getElementById("txt").value.length == 0) {
+    document.getElementById("name_error").innerText = "* Name Required"
+  }
+  else {
+    var firstname = document.getElementById("txt").value
+    localStorage.setItem("textvalue",firstname)
+    let url = "game.html"
+    window.location = url
+  }
+  return false
 }
 
 
  togglePopup = () => {
-    document.getElementById("popup-1").classList.toggle("active");
+    document.getElementById("popup-1").classList.toggle("active")
   }
 
 
  playMe = () => {
 
-  var audio = document.getElementById('testAudio');
+  let audio = document.getElementById('testAudio')
 
     if(this.className == 'is-playing'){
-      this.className = "";
-      document.getElementById("playAudio").innerHTML = "Play &#9658;";
-      document.getElementById("home_audio").src = "assets/img/no-sound.png";
-      audio.pause();
+      this.className = ""
+      document.getElementById("playAudio").innerHTML = "Play &#9658;"
+      document.getElementById("home_audio").src = "assets/img/sound-off.png"
+      audio.pause()
       localStorage.setItem(AUDIO_KEY, "false")
     }else{
-      this.className = "is-playing";
-      document.getElementById("playAudio").innerHTML = "Pause &#9208;";
-      document.getElementById("home_audio").src = "assets/img/volume.png";
-      audio.play();
-      localStorage.setItem(AUDIO_KEY, "true");
+      this.className = "is-playing"
+      document.getElementById("playAudio").innerHTML = "Pause &#9208;"
+      document.getElementById("home_audio").src = "assets/img/sound-on.png"
+      audio.play()
+      localStorage.setItem(AUDIO_KEY, "true")
     }
 }
 
